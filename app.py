d8bb076ed775302fac26c0ed5b704e5eaa75309a
rocktronica <tommy@iamnotagoodartist.com>
2015-12-13 12:31:53 -0800

    Externalize settings

--- app.py
+++ app.py
@@ -5,16 +5,26 @@ from subprocess import call
 import os
 from glob import glob
 
+from ConfigParser import ConfigParser
+config = ConfigParser()
+config.read('settings.ini')
+
+HOST = config.get('settings', 'host')
+DURATION_MINUTES = int(config.get('settings', 'duration_minutes'))
+BREAKFAST_HOUR = int(config.get('settings', 'breakfast_hour'))
+DINNER_HOUR = int(config.get('settings', 'dinner_hour'))
+SLEEP = int(config.get('settings', 'sleep'))
+
 def make_time(hour=0, minute=0):
     today = datetime.now()
     return today.replace(hour=hour, minute=minute, second=0, microsecond=0)
 
 ranges = [{
-    'start': make_time(7),
-    'stop': make_time(7, 8)
+    'start': make_time(BREAKFAST_HOUR),
+    'stop': make_time(BREAKFAST_HOUR, DURATION_MINUTES)
 },{
-    'start': make_time(7 + 12),
-    'stop': make_time(7 + 12, 8)
+    'start': make_time(DINNER_HOUR),
+    'stop': make_time(DINNER_HOUR, DURATION_MINUTES)
 }]
 
 def within_range(now, start, stop):
@@ -42,7 +52,7 @@ def delete_images():
     os.system('rm -f images/*')
 
 def download_image():
-    os.system('curl -# -L --compressed http://10.0.0.2/photo > "images/$(date +%s).jpg"')
+    os.system('curl -# -L --compressed ' + HOST + '/photo > "images/$(date +%s).jpg"')
 
 def output_gif(filename):
     os.system('ffmpeg -pattern_type glob -i \'images/*.jpg\' -r 30 -vf scale=320:-1 "' + filename + '"')
@@ -63,7 +73,6 @@ if __name__ == '__main__':
 
         if on:
             download_image()
-            print
         else:
             images = get_image_slugs()
 
@@ -90,4 +99,4 @@ if __name__ == '__main__':
                     print '------------'
                     print
 
-        time.sleep(1)
+        time.sleep(SLEEP)

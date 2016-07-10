11b9c7060c4cd12567d06a85610903be3526e4b5
rocktronica <tommy@iamnotagoodartist.com>
2016-07-10 09:01:34 -0700

    Update start script
    - Fix sudo sudo typo
    - Add camcamcam

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ cd ~/feedergif
 
 app_args=$(echo ${@:1})
 
-screen -S feedergif_app -dm bash -c "sudo python app.py $app_args"
-screen -S feedergif_server -dm bash -c "sudo sudo python server.py"
+screen -S feedergif -dm bash -c "sudo python app.py  --url http://0.0.0.0:8081/capture $app_args"
+screen -S server -dm bash -c "sudo python server.py"
+screen -S camcamcam -dm bash -c "sudo python ~/camcamcam/app.py --host 0.0.0.0 --port 8081 --width 400 --height 300 --rotate 270"
